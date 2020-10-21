9b7b05e6036bc61e1591eb74f33f223858a23114
Muskan Jaiswal <muskanjaiswal710@gmail.com>
2020-10-21 21:54:15 +0530

    'correctedlast'

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -2,7 +2,7 @@
 
 import{ COMMENTS } from '../shared/comments';
 
-export const Comments ={state = COMMENTS, action} => {
+export const Comments =(state = COMMENTS, action) => {
     switch(action.type) {
         default:
             return state;
--- src/redux/leaders.js
+++ src/redux/leaders.js
@@ -1,6 +1,6 @@
 import { LEADERS } from '../shared/leaders';
 
-export const leaders ={state = LEADERS, action} => {
+export const Leaders =(state = LEADERS, action) => {
     switch(action.type) {
         default:
             return state;
