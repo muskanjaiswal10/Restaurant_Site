16c1291f4cdd435d14ac0e2c02a5fe8a49fcbd43
Muskan Jaiswal <muskanjaiswal710@gmail.com>
2020-10-22 01:10:48 +0530

    'react_thunk'

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import{ Link } from 'react-router-dom';
+import {Loading} from './LoadingComponent';
      function RenderMenuItem({dish,onClick}){
          return(
             <Card >
@@ -16,7 +17,7 @@ import{ Link } from 'react-router-dom';
          )
      }
    const Menu =(props) => {
-    const menu = props.dishes.map(dish =>{
+    const menu = props.dishes.dishes.map(dish =>{
         return (
             
                 <div key={dish.id}  className="col-12 col-md-5 m-1">
@@ -24,6 +25,26 @@ import{ Link } from 'react-router-dom';
                 </div>
         );
     });
+    if(props.dishes.isLoading){
+    return(
+        <div className="container">
+            <div className="row">
+                <Loading/>
+            </div>
+        </div>
+       );
+    }
+   else if(props.dishes.errMess){
+       return(
+           <div className="container">
+               <div className="row">
+               <h4>{props.dishes.errMess}</h4>
+               </div>
+           </div>
+          );
+
+   }
+   else
     return (
         <div className="container">
             <div className="row">
